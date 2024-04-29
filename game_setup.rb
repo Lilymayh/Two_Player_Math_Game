a6ce321b3842cb05c2af1410e90e6a34bae13fa8
Lily <lmayhenderson@yahoo.com>
2024-04-29 13:20:36 -0700

    Game setup.

---
+++ game_setup.rb
@@ -0,0 +1,23 @@
+=begin
+Classes:
+
+questions: generates random math additions with two random numbers
+- initilize: initialize with no arguments
+- methods: generate_question
+
+players: represents two players, their scores, remaining lives, and their ability to answer questions
+- info: two names
+- initialize: payer names and number of lives
+- methods: answer_question, lose-live, game_over
+
+score: keep track of each players score
+- info: scores of each player
+- initialize: names of players
+- methods: update_score, show_score
+
+Methods:
+-game loop method
+-player methods
+-Question generation method: 
+
+=end
